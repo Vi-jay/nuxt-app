148755db9ad91dc135ddd9aa0155977e5b41d7be
VijayAdam <765139628@qq.com>
2022-11-17 16:54:53 +0800

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="Index">
     <home-nav />
+    <p>{{ data }}</p>
     <nuxt-link to="/page2">page2 </nuxt-link>
   </div>
 </template>
 <script setup lang="ts">
 import { sleepAsync } from "~/utils";
 import HomeNav from "~/components/home-nav.vue";
+
+const { pending, data } = await useLazyAsyncData("Test", async () => {
+  console.log("执行了");
+  return await $fetch("http://lumtest.com/myip.json");
+});
+console.log(data.value);
 </script>
 <style lang="scss">
 @import "assets/common.scss";
