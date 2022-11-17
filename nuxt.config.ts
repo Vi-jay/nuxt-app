accd956b8adc98d3821f5ef60f7448fd0e3c5035
VijayAdam <765139628@qq.com>
2022-11-17 12:07:11 +0800

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
+import legacy from "@vitejs/plugin-legacy";
+
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
   app: {
@@ -7,6 +9,13 @@ export default defineNuxtConfig({
         lang: "zh-cn",
       },
     },
+  },
+  vite: {
+    plugins: [
+      legacy({
+        targets: ["defaults"],
+      }),
+    ],
   },
   css: ["~/assets/theme.scss"],
   components: false,
